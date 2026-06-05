9fbfb16b575bafe27b38cfc52f07f6761371ae8a
Esteban Dev <dominguezesteban1020@gmail.com>
2026-06-05 01:23:41 -0500

    Ajustes en conexión socket.io de notificaciones.services.js

--- src/services/notificaciones.service.js
+++ src/services/notificaciones.service.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000', {
+const socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000', {
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
